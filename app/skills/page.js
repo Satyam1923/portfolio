c9e159b1c4b48f7d615337d915b1c694131a964b
Satyam Rathor <satyamr1923@gmail.com>
2025-04-12 21:42:57 +0530

    fix for mobile

--- app/skills/page.js
+++ app/skills/page.js
@@ -6,32 +6,34 @@ import {
 } from "react-icons/fa";
 import {
     SiC, SiCplusplus, SiPython, SiFirebase, SiExpress, SiTailwindcss,
-    SiRedux, SiUbuntu, SiPostman, SiMysql, SiArduino
+    SiRedux, SiUbuntu, SiPostman, SiMysql, SiArduino,SiPostgresql,SiNextdotjs
 } from "react-icons/si";
 import { motion } from "framer-motion";
 import { Playfair_Display } from 'next/font/google';
 const playfair = Playfair_Display({ subsets: ['latin'], weight: ['400', '700'] });
 
 const skills = [
-    { icon: <FaHtml5 className="text-orange-500" />, name: "HTML", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
-    { icon: <FaCss3Alt className="text-blue-500" />, name: "CSS", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
-    { icon: <FaJs className="text-yellow-400" />, name: "JavaScript", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
     { icon: <SiC className="text-blue-700" />, name: "C", link: "https://en.wikipedia.org/wiki/C_(programming_language)" },
     { icon: <SiCplusplus className="text-blue-500" />, name: "C++", link: "https://isocpp.org/" },
     { icon: <SiPython className="text-yellow-400" />, name: "Python", link: "https://www.python.org/" },
+    
+    { icon: <FaHtml5 className="text-orange-500" />, name: "HTML", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+    { icon: <FaCss3Alt className="text-blue-500" />, name: "CSS", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+    { icon: <FaJs className="text-yellow-400" />, name: "JavaScript", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
     { icon: <FaReact className="text-blue-400" />, name: "React.js", link: "https://react.dev/" },
+    { icon: <SiNextdotjs className="text-white" />, name: "Next.js", link: "https://nextjs.org/" },
     { icon: <SiRedux className="text-purple-600" />, name: "Redux Toolkit", link: "https://redux-toolkit.js.org/" },
-    { icon: <SiExpress className="text-gray-400" />, name: "Express.js", link: "https://expressjs.com/" },
-    { icon: <FaNodeJs className="text-green-500" />, name: "Node.js", link: "https://nodejs.org/" },
     { icon: <FaBootstrap className="text-purple-600" />, name: "Bootstrap", link: "https://getbootstrap.com/" },
     { icon: <SiTailwindcss className="text-teal-400" />, name: "Tailwind CSS", link: "https://tailwindcss.com/" },
+    { icon: <FaNodeJs className="text-green-500" />, name: "Node.js", link: "https://nodejs.org/" },
+    { icon: <SiExpress className="text-gray-400" />, name: "Express.js", link: "https://expressjs.com/" },
+    { icon: <SiPostgresql className="text-blue-600" />, name: "PostgreSQL", link: "https://www.postgresql.org/" },
     { icon: <SiFirebase className="text-yellow-500" />, name: "Firebase", link: "https://firebase.google.com/" },
     { icon: <FaGitAlt className="text-red-600" />, name: "Git", link: "https://git-scm.com/" },
     { icon: <FaGithub className="text-white" />, name: "GitHub", link: "https://github.com/" },
-    { icon: <SiUbuntu className="text-orange-500" />, name: "Linux", link: "https://ubuntu.com/" },
     { icon: <SiPostman className="text-orange-400" />, name: "Postman", link: "https://www.postman.com/" },
-    { icon: <SiMysql className="text-blue-600" />, name: "MySQL", link: "https://www.mysql.com/" },
-    { icon: <SiArduino className="text-teal-600 " />, name: "Arduino", link: "https://www.arduino.cc/" }
+    { icon: <SiUbuntu className="text-orange-500" />, name: "Linux", link: "https://ubuntu.com/" },
+    { icon: <SiArduino className="text-teal-600" />, name: "Arduino", link: "https://www.arduino.cc/" }
 ];
 
 export default function Skills() {
